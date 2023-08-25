a9501d1d5fa1ebef688189808343cbbde3d04551
Jared Carlson <jaredrcarlson@gmail.com>
2023-08-25 11:16:06 -0600

    env init

--- keepr.client/src/env.js
+++ keepr.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-eux5v4apdr8nm4hz.us.auth0.com'
+export const audience = 'classroom2'
+export const clientId = 'Rur2HYRUEOc9CFa5kFMGh67L2LmsuzQH'
