c600c1c3c1c18f0e013c5a2ab9ea2954f0c3ddbf
Jared Carlson <jaredrcarlson@gmail.com>
2023-09-20 22:04:16 -0600

    vite prod config update

--- keepr.client/vite.config.js
+++ keepr.client/vite.config.js
@@ -11,7 +11,10 @@ export default defineConfig({
   server: {
     host: true,
     port: 8081,
-    strictPort: true
+    strictPort: true,
+    hmr: {
+      clientPort: 443
+    }
   },
   base: '/keepr/'
 })
