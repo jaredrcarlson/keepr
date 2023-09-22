b3ae2769743466b078b97f40d05f7f892fbf21d8
Jared Carlson <jaredrcarlson@gmail.com>
2023-09-22 15:58:14 -0600

    auth update

--- keepr.client/src/env.js
+++ keepr.client/src/env.js
@@ -3,5 +3,5 @@ export const baseURL = dev ? 'https://localhost:7045' : ''
 export const servePath = dev ? '' : '/keepr/'
 export const useSockets = false
 export const domain = 'dev-eux5v4apdr8nm4hz.us.auth0.com'
-export const audience = 'classroom2'
-export const clientId = 'Rur2HYRUEOc9CFa5kFMGh67L2LmsuzQH'
+export const audience = 'https://jaredcarlson.dev'
+export const clientId = 'g52d0A3LtKilSekymUMshelTjAs7WGes'
